675f23f2ff8271db42bf6bf6c7fa957240dcc5b8
fsrob <robert.newman@foresite.com>
2022-01-27 17:36:28 +0000

    - Updated tool checker to show all missing tools rather than just the first one - Remove 127.0.0.1 from internal IP addresses list - Added check to ensure that `target.ip` file is populated before running tests - Running without arguments now shows the help screen - Updated getopts to allow long options - Added option to specify the network interface for masscan to use - added new command line arguments: --tcpportrange --udpportrange --nmap-minhost --nmap-minrate --masscan-maxrate --masscan-interface --outputdir - Script no longer asks for NMAP options. Instead, these should be specified on the command line - Replaced "press enter to continue" after IP addresses are displayed to a 5 second countdown timer - Output directory is now based on the current date/time and is placed in the current directory - Added option to specify the output directory (handy if you just want to run the nmap to CSV script) - Fixed: You can now run the script without being in the script directory - Fixed: masscan now scans UDP ports as well as TCP ports - Fixed: Summary unique ports is wrong if it finds the same port open for both udp and tcp (e.g. UDP/53 and TCP/53 show as one unique port)

--- parsers.sh
+++ parsers.sh
@@ -1,12 +1,12 @@
 #-- combining masscan and nmap results
 combiner(){
     echo -e "\n[${GREEN}+${RESET}] Combining ${YELLOW}nmap${RESET} and ${YELLOW}masscan${RESET} scans"
-    touch alive.ip
-    touch masscan/alive.ip
-    cp masscan/scans/* scans
-    cp nmap/scans/* scans
-    cat masscan/scans/$PORTRANGE.gnmap | head -n -1 | tail -n +3 | cut -d ' ' -f 2 | sort -u > masscan/alive.ip
-    cat masscan/alive.ip nmap/alive.ip | sort -u >> alive.ip
+    touch "$OUTPUTDIR/alive.ip"
+    touch "$OUTPUTDIR/masscan/alive.ip"
+    cp "$OUTPUTDIR/masscan/scans/"* "$OUTPUTDIR/scans"
+    cp "$OUTPUTDIR/nmap/scans/"* "$OUTPUTDIR/scans"
+    cat "$OUTPUTDIR/masscan/scans/masscan.gnmap" | head -n -1 | tail -n +3 | cut -d ' ' -f 3 | sort -u > "$OUTPUTDIR/masscan/alive.ip"
+    cat "$OUTPUTDIR/masscan/alive.ip" "$OUTPUTDIR/nmap/alive.ip" | sort -u >> "$OUTPUTDIR/alive.ip"
 }
 
 #progress bar
@@ -23,19 +23,19 @@ parser(){
  
     for n in $(seq $MINPORT $MAXPORT);   
     do
-	if [ $(cat scans/*.gnmap | egrep " $n\/open\/tcp/" | cut -d " " -f 2 | wc -l) -eq '0' ];
+	if [ $(cat "$OUTPUTDIR/scans/"*.gnmap | egrep " $n\/open\/tcp/" | cut -d " " -f 2 | wc -l) -eq '0' ];
 	then
 	    prog "$n" out of $MAXPORT TCP ports...
 	   # sleep .1 	
 	else
-	    cat scans/*.gnmap | egrep " $n\/open\/tcp/" | cut -d " " -f 2 >> open-ports/$n.txt
+	    cat "$OUTPUTDIR/scans/"*.gnmap | egrep " $n\/open\/tcp/" | cut -d " " -f 2 >> "$OUTPUTDIR/open-ports/$n.txt"
 	fi
-	if [ $(cat scans/*.gnmap | egrep " $n\/open\/udp/" | cut -d " " -f 2 | wc -l) -eq '0' ];
+	if [ $(cat "$OUTPUTDIR/scans/"*.gnmap | egrep " $n\/open\/udp/" | cut -d " " -f 2 | wc -l) -eq '0' ];
 	then
 	    prog "$n" out of $MAXPORT UDP ports...
 	   # sleep .1 
 	else
-	    cat scans/*.gnmap | egrep " $n\/open\/udp/" | cut -d " " -f 2 >> open-ports/$n.txt
+	    cat "$OUTPUTDIR/scans/"*.gnmap | egrep " $n\/open\/udp/" | cut -d " " -f 2 >> "$OUTPUTDIR/open-ports/$n.txt"
 	fi
     done
 }
@@ -48,20 +48,22 @@ csvParser(){
 	exit 1
     else
     	wget https://raw.githubusercontent.com/TDSSEC/Nmap-Scan-to-CSV/master/nmap_xml_parser.py 
-	$(which python3) nmap_xml_parser.py -f scans/*.xml -csv host-info.csv
+	$(which python3) nmap_xml_parser.py -f "$OUTPUTDIR/scans/"*.xml -csv "$OUTPUTDIR/host-info.csv"
     fi
 }
 #-- summary
 summary(){
     echo -e "\n[${GREEN}+${RESET}] Generating a summary of the scans..."
-    for ip in $(cat ./alive.ip); do
-	echo -e $ip > ./open-ports/$ip.txt
-	awk \/$ip\/ masscan/scans/$PORTRANGE.gnmap | egrep -o '*[0-9]*/open/*[tcp/udp]*/' | sort | uniq | awk -F '/' '{print $1"/"$3}' >> ./open-ports/$ip.txt
-	awk \/$ip\/ nmap/scans/portscan.gnmap | egrep -o '*[0-9]*/open/*[tcp/udp]*/' | sort | uniq | awk -F '/' '{print $1"/"$3}' >> ./open-ports/$ip.txt
+    for ip in $(cat "$OUTPUTDIR/alive.ip"); do
+	echo -e $ip > "$OUTPUTDIR/open-ports/$ip.txt"
+	awk \/$ip\/ "$OUTPUTDIR/masscan/scans/masscan.gnmap" | egrep -o '*[0-9]*/open/*[tcp/udp]*/' | sort | uniq | awk -F '/' '{print $1"/"$3}' >> "$OUTPUTDIR/tempports.tmp"
+	awk \/$ip\/ "$OUTPUTDIR/nmap/scans/portscan.gnmap" | egrep -o '*[0-9]*/open/*[tcp/udp]*/' | sort | uniq | awk -F '/' '{print $1"/"$3}' >> "$OUTPUTDIR/tempports.tmp"
+    cat "$OUTPUTDIR/tempports.tmp" | sort -u >> "$OUTPUTDIR/open-ports/$ip.txt"
+    rm -f "$OUTPUTDIR/tempports.tmp"
     done
-    echo -e "\n[${GREEN}+${RESET}] there are $(cat ./alive.ip | wc -l ) ${YELLOW}alive hosts${RESET} and $(egrep -o '[0-9]*/open/' scans/*.gnmap | cut -d ':' -f 2 | sort | uniq | wc -l) ${YELLOW}unique ports/services${RESET}" | tee -a discovered_ports.txt
+    echo -e "\n[${GREEN}+${RESET}] there are $(cat "$OUTPUTDIR/alive.ip" | wc -l ) ${YELLOW}alive hosts${RESET} and $(egrep -o '[0-9]*/open/*[tcp/udp]*/' "$OUTPUTDIR/scans/"*.gnmap | cut -d ':' -f 2 | sort | uniq | wc -l) ${YELLOW}unique ports/services${RESET}" | tee -a "$OUTPUTDIR/discovered_ports.txt"
 }
 
 summaryPingSweep(){
-    echo -e "\n[${GREEN}+${RESET}] ${YELLOW}There are $(cat nmap/alive.ip | wc -l ) alive hosts${RESET}"
+    echo -e "\n[${GREEN}+${RESET}] ${YELLOW}There are $(cat "$OUTPUTDIR/nmap/alive.ip" | wc -l ) alive hosts${RESET}"
 }
