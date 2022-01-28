439a369b8a4e9c3275a3d64658106abd8dd3729a
TDSSEC <20561984+TDSSEC@users.noreply.github.com>
2022-01-28 23:55:54 +0000

    Update parsers.sh
    - MAXPORT & MINPORT removed. - standard sequence 1-65535 to cover all ports when parsing
    - If statement added in the event masscan was not a chosen option at runtime and file does not exist
    - Removal of python3 script for CSV creation.

--- parsers.sh
+++ parsers.sh
@@ -13,53 +13,60 @@ combiner(){
 prog() {
     local w=80 p=$1;  shift
     # create a string of spaces, then change them to dots
-    printf -v dots "%*s" "$(( $p*$w/$MAXPORT ))" ""; dots=${dots// /#};
+    printf -v dots "%*s" "$(( $p*$w/65535 ))" ""; dots=${dots// /#};
     # print those dots on a fixed-width space plus the percentage etc. 
     printf "\r\e[K|%-*s| %3d  %s" "$w" "$dots" "$p" "$*"; 
 }
 
 parser(){
     echo -e "\n[${GREEN}+${RESET}] Running ${YELLOW}parser${RESET} for ${YELLOW}nse${RESET} scans"
- 
-    for n in $(seq $MINPORT $MAXPORT);   
+
+    for n in $(seq 1 65535);
     do
 	if [ $(cat "$OUTPUTDIR/scans/"*.gnmap | egrep " $n\/open\/tcp/" | cut -d " " -f 2 | wc -l) -eq '0' ];
 	then
-	    prog "$n" out of $MAXPORT TCP ports...
-	   # sleep .1 	
+	    prog "$n" out of 65535 TCP ports...
+	   # sleep .1
 	else
 	    cat "$OUTPUTDIR/scans/"*.gnmap | egrep " $n\/open\/tcp/" | cut -d " " -f 2 >> "$OUTPUTDIR/open-ports/$n.txt"
 	fi
 	if [ $(cat "$OUTPUTDIR/scans/"*.gnmap | egrep " $n\/open\/udp/" | cut -d " " -f 2 | wc -l) -eq '0' ];
 	then
-	    prog "$n" out of $MAXPORT UDP ports...
-	   # sleep .1 
+	    prog "$n" out of 65535 UDP ports...
+	   # sleep .1
 	else
 	    cat "$OUTPUTDIR/scans/"*.gnmap | egrep " $n\/open\/udp/" | cut -d " " -f 2 >> "$OUTPUTDIR/open-ports/$n.txt"
 	fi
     done
 }
-#nmap to CSV - requires python3
-#https://github.com/laconicwolf/Nmap-Scan-to-CSV
+#nmap to CSV - using style sheet and xsltproc
 csvParser(){
     echo -e "\n[${GREEN}+${RESET}] Running ${YELLOW}parser${RESET} for ${YELLOW}NMAP XML to CSV${RESET}"
-    if ! hash python3; then
-	echo "python3 is not installed"
-	exit 1
-    else
-    	wget https://raw.githubusercontent.com/TDSSEC/Nmap-Scan-to-CSV/master/nmap_xml_parser.py 
-	$(which python3) nmap_xml_parser.py -f "$OUTPUTDIR/scans/"*.xml -csv "$OUTPUTDIR/host-info.csv"
-    fi
+    xsltproc xml-to-csv.xsl "$OUTPUTDIR/scans/"*.xml --output "$OUTPUTDIR/csv_and_html_files/nmap-results.csv"
 }
+
+#nmap to HTML using xsltproc
+htmlParser(){
+    echo -e "\n[${GREEN}+${RESET}] Running ${YELLOW}parser${RESET} for ${YELLOW}NMAP XML to HTML${RESET}"
+    xsltproc "$OUTPUTDIR/scans/"*.xml -o "$OUTPUTDIR/csv_and_html_files/nmap-results.html"
+}
+
 #-- summary
 summary(){
+    MASSCAN_FILE="$OUTPUTDIR/masscan/scans/masscan.gnmap"
     echo -e "\n[${GREEN}+${RESET}] Generating a summary of the scans..."
     for ip in $(cat "$OUTPUTDIR/alive.ip"); do
 	echo -e $ip > "$OUTPUTDIR/open-ports/$ip.txt"
-	awk \/$ip\/ "$OUTPUTDIR/masscan/scans/masscan.gnmap" | egrep -o '*[0-9]*/open/*[tcp/udp]*/' | sort | uniq | awk -F '/' '{print $1"/"$3}' >> "$OUTPUTDIR/tempports.tmp"
-	awk \/$ip\/ "$OUTPUTDIR/nmap/scans/portscan.gnmap" | egrep -o '*[0-9]*/open/*[tcp/udp]*/' | sort | uniq | awk -F '/' '{print $1"/"$3}' >> "$OUTPUTDIR/tempports.tmp"
-    cat "$OUTPUTDIR/tempports.tmp" | sort -u >> "$OUTPUTDIR/open-ports/$ip.txt"
-    rm -f "$OUTPUTDIR/tempports.tmp"
+	if [ -f "$MASSCAN_FILE" ]; then #if masscan was executed, check both masscan and nmap
+            awk \/$ip\/ "$OUTPUTDIR/masscan/scans/masscan.gnmap" | egrep -o '*[0-9]*/open/*[tcp/udp]*/' | sort | uniq | awk -F '/' '{print $1"/"$3}' >> "$OUTPUTDIR/tempports.tmp"
+	    awk \/$ip\/ "$OUTPUTDIR/nmap/scans/portscan.gnmap" | egrep -o '*[0-9]*/open/*[tcp/udp]*/' | sort | uniq | awk -F '/' '{print $1"/"$3}' >> "$OUTPUTDIR/tempports.tmp"
+	    cat "$OUTPUTDIR/tempports.tmp" | sort -u >> "$OUTPUTDIR/open-ports/$ip.txt"
+	    rm -f "$OUTPUTDIR/tempports.tmp"
+	else #just check nmap files
+	    awk \/$ip\/ "$OUTPUTDIR/nmap/scans/portscan.gnmap" | egrep -o '*[0-9]*/open/*[tcp/udp]*/' | sort | uniq | awk -F '/' '{print $1"/"$3}' >> "$OUTPUTDIR/tempports.tmp"
+            cat "$OUTPUTDIR/tempports.tmp" | sort -u >> "$OUTPUTDIR/open-ports/$ip.txt"
+            rm -f "$OUTPUTDIR/tempports.tmp"
+	fi
     done
     echo -e "\n[${GREEN}+${RESET}] there are $(cat "$OUTPUTDIR/alive.ip" | wc -l ) ${YELLOW}alive hosts${RESET} and $(egrep -o '[0-9]*/open/*[tcp/udp]*/' "$OUTPUTDIR/scans/"*.gnmap | cut -d ':' -f 2 | sort | uniq | wc -l) ${YELLOW}unique ports/services${RESET}" | tee -a "$OUTPUTDIR/discovered_ports.txt"
 }
